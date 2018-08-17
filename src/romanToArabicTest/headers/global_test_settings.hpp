51da53555ac6afec8bbc6b614249d0fc1f8f4073
BrainStone <yannick@tedworld.de>
2018-08-17 23:24:13 +0200

    Added base test code

---
+++ src/romanToArabicTest/headers/global_test_settings.hpp
@@ -0,0 +1,7 @@
+//
+// Copyright (c) 2018 Yannick Schinko. All rights reserved.
+// Licensed under the GPLv3 License. See LICENSE file in the project root for full license information.
+//
+#pragma once
+
+#include <gtest/gtest.h>
