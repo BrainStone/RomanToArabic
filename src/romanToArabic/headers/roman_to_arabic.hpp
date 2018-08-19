4568b40d11da1b06d7cc3191c2a6351bd0df12c0
BrainStone <yannick@tedworld.de>
2018-08-19 10:42:47 +0200

    Remove unused Makro

--- src/romanToArabic/headers/roman_to_arabic.hpp
+++ src/romanToArabic/headers/roman_to_arabic.hpp
@@ -27,18 +27,6 @@ namespace roman_to_arabic {
 #	define __ROMAN_TO_ARABIC_CPP11 0
 #endif
 
-/// Visual Studio's partial C++11 support is a pain in the ass!
-/// <code>constexpr</code> only supported on VS 2015 and above!
-#if !defined(__ROMAN_TO_ARABIC_CONSTEXPR) && \
-	((__cplusplus >= 201103L) \
-	|| (defined(_MSC_VER) && (_MSC_VER >= 1900)))
-/// <code>constexpr</code> available
-#	define __ROMAN_TO_ARABIC_CONSTEXPR constexpr
-#else
-/// <code>constexpr</code> not available. Using <code>const</code> instead
-#	define __ROMAN_TO_ARABIC_CONSTEXPR const
-#endif
-
 // Now the other includes
 #include "roman_to_arabic/convert.hpp"
 #include "roman_to_arabic/digits.hpp"
